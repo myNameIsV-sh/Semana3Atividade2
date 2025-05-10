23058e0bcc811a0ffb6cd0c4c57a52b2297861d1
V <victor.gustavo.c@proton.me>
2025-05-09 21:55:34 -0300

    VERIFIED: algoritmo analisado e explicado

--- algoritmo1.js
+++ algoritmo1.js
@@ -7,13 +7,28 @@ const tamanho = prompt ('Digite o tamanho da lista de números');
 // Referência: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/fill
 const lista = new Array(tamanho).fill(null);
 
+// 3. Aqui a lista de tamanho N após ser preenchida com valores nulos será "mapeada"
+// 3.1. O primeiro argumento `item` representa o valor atual, enquanto o segundo
+// argumento `index` representa o índice desse valor
 lista.map(function (item, index) {
-    return index + 1;
-})
+    // 3.2. Apenas o valor contido em `index` será somado, gerando essa saída
+    return index + 1; // Exemplo com 5 elementos: [1, 2, 3, 4, 5]
+}) // Método `map` se encerra..
+    // 4.1. `reduce` irá agrupar todos os valores contidos em apenas uma variável
+    // 4.1.2. Após a função ser declarada, o primeiro argumento a ser informado é o acumulador
+    // O valor a ser definido será o resultado da função (Callback) anterior; Exemplo: [1, 2, 3, 4, 5]
 .reduce(function (acumulador, item) {
-    return acumulador * item;
+    // 4.1.2. O retorno será a multiplicação entre o valor acumulado e o `item`, sendo item
+    // a representação do índice atual que está sendo processado
+    return acumulador * item; // 120
 });
 
+// 4. O procedimento aqui é quase o mesmo — o Array será mapeado e preenchido com valores nulos.
 new Array(tamanho).fill(null)
+    // 4.1. Entretanto, no método `map` como não há retorno claro aqui,
+    //  todo o Array será preenchido com números 1.
+    // 4.1.1 `+1` é um operador unário de adição, na prática o interpretador tenta converter o valor
+    // para um inteiro, porém, como esse valor já é inteiro, então não acontece nada.
     .map((item, index) => + 1)
-    .reduce((acumulador, item) => acumulador + item);
+    // 4.2. E aqui os valores serão reduzidos em para um só
+    .reduce((acumulador, item) => acumulador + item); // 1
