33e8615aa75369ef348069df5c397173f1dc6fee
V <victor.gustavo.c@proton.me>
2025-05-09 22:39:13 -0300

    VERIFIED: algoritmo2.js analisado, sem verificações para serem feitas, não vejo como detalhar ainda mais

--- algoritmo2.js
+++ algoritmo2.js
@@ -4,17 +4,20 @@ const letras = [
     ['c', 'd', 'f'],
     ['d', 'f', 'g'],
 ];
-
 // 2. O método `.flat()` irá criar um novo Array com os valores concatenados.
 // 2.1. Nota: por padrão o método achata a "primeira" (1) camada do Array
 // Referência: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
 let resultado = letras.flat().reduce((objeto, numero) => {
+    // 2.2. Após o Array ser achatado e ser "transformado" em um objeto
+    // o reduce irá "contar" todas as vezes que uma letra se repetir,
+    // importante salientar que as verificações são feitas de maneira implícita
     if (objeto[numero]) {
+        // 2.2.1. `objeto` é o nosso acumulador, `numero` representa a letra atual
+        // 2.2.2. Se a letra já fora encontrada, o contador(objeto) será incrementado em 1
         objeto[numero] += 1;
     } else {
+        // 2.2.3. Caso contrário, a "letra" será adicionada a contagem
         objeto[numero] = 1;
     }
-    return objeto;
+    return objeto; // { a: 1, b: 1, c: 2, d: 2, f: 2, g: 1 }
 }, {});
-
-console.log(resultado); // { a: 1, b: 1, c: 2, d: 2, f: 2, g: 1 }
